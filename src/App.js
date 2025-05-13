b20f522293afd7d40a60cce57237b5d67801c7db
hannan-jsx <hannan@geeksofkolachi.com>
2025-05-13 12:41:42 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
-import { Suspense, lazy, useEffect, useState } from "react";
-import "react-image-lightbox/style.css";
-import "react-modern-drawer/dist/index.css";
-import "react-phone-number-input/style.css";
-import "react-quill/dist/quill.snow.css";
-import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
-import "./assets/Styles/style.css";
-import "./assets/Styles/table.css";
-import { Get } from "./Axios/AxiosFunctions";
-import { Loader } from "./components/Core/Loader";
-import { BaseURL } from "./config/apiUrl";
-import ScrollToTop from "./Helper/ScrollToTop";
-import { updateUser } from "./store/auth/authSlice";
-import { saveNewNotification } from "./store/common/commonSlice";
-import ProtectedRouter from "./Helper/ProtectedRoute";
-import BeforeLoginRoute from "./Helper/BeforeLoginRoute";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { Suspense, lazy, useEffect, useState } from 'react';
+import 'react-modern-drawer/dist/index.css';
+import 'react-phone-number-input/style.css';
+import 'react-quill/dist/quill.snow.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import './assets/Styles/style.css';
+import './assets/Styles/table.css';
+import { Get } from './Axios/AxiosFunctions';
+import { Loader } from './components/Core/Loader';
+import { BaseURL } from './config/apiUrl';
+import ScrollToTop from './Helper/ScrollToTop';
+import { updateUser } from './store/auth/authSlice';
+import { saveNewNotification } from './store/common/commonSlice';
+import ProtectedRouter from './Helper/ProtectedRoute';
+import BeforeLoginRoute from './Helper/BeforeLoginRoute';
 
-const Login = lazy(() => import("./pages/Login"));
-const SignUp = lazy(() => import("./pages/SignUp"));
+const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/SignUp'));
 
-const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
 
-const Report = lazy(() => import("./pages/Report"));
-const ViewReport = lazy(() => import("./pages/Report/ViewReport"));
-const AddReport = lazy(() => import("./pages/Report/AddReport"));
+const Report = lazy(() => import('./pages/Report'));
+const ViewReport = lazy(() => import('./pages/Report/ViewReport'));
+const AddReport = lazy(() => import('./pages/Report/AddReport'));
 
-const ProfileSettings = lazy(() => import("./pages/ProfileSettings"));
-const ChangePassword = lazy(() => import("./pages/ChangePassword"));
-const Notifications = lazy(() => import("./pages/Notifications"));
-const NotFound = lazy(() => import("./pages/NotFound"));
+const ProfileSettings = lazy(() => import('./pages/ProfileSettings'));
+const ChangePassword = lazy(() => import('./pages/ChangePassword'));
+const Notifications = lazy(() => import('./pages/Notifications'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -40,7 +39,7 @@ function App() {
   const dispatch = useDispatch();
 
   const getMe = async () => {
-    const apiUrl = BaseURL("users/get-me");
+    const apiUrl = BaseURL('users/get-me');
     // const notificationUrl = BaseURL("notifications/unread");
     setIsLoading(true);
     const [userRes] = await Promise.allSettled([
@@ -63,7 +62,7 @@ function App() {
   }, [isLogin]);
 
   if (isLoading) {
-    return <Loader className="vh-100" />;
+    return <Loader className='vh-100' />;
   }
 
   return (
@@ -71,54 +70,54 @@ function App() {
       <ToastContainer />
       <BrowserRouter>
         <ScrollToTop />
-        <Suspense fallback={<Loader className={"vh-100"} />}>
+        <Suspense fallback={<Loader className={'vh-100'} />}>
           <Routes>
             <Route
-              path="/login"
+              path='/login'
               exact
               element={<BeforeLoginRoute element={<Login />} />}
               // element={<Login />}
             />
-            <Route path="/sign-up" exact element={<SignUp />} />
+            <Route path='/sign-up' exact element={<SignUp />} />
             <Route
-              path="/"
+              path='/'
               exact
               element={<ProtectedRouter element={<Dashboard />} />}
-            />{" "}
+            />{' '}
             <Route
-              path="/report"
+              path='/report'
               exact
               element={<ProtectedRouter element={<Report />} />}
-            />{" "}
+            />{' '}
             <Route
-              path="/profile-settings"
+              path='/profile-settings'
               exact
               element={<ProtectedRouter element={<ProfileSettings />} />}
-            />{" "}
+            />{' '}
             <Route
-              path="/change-password"
+              path='/change-password'
               exact
               element={<ProtectedRouter element={<ChangePassword />} />}
               // element={<ChangePassword />}
             />
             <Route
-              path="/notifications"
+              path='/notifications'
               exact
               // element={<ProtectedRouter element={<Notifications />} />}
               element={<Notifications />}
             />
             <Route
-              path="/report/add"
+              path='/report/add'
               exact
               element={<ProtectedRouter element={<AddReport />} />}
               // element={<AddReport />}
             />
             <Route
-              path="/report/:slug"
+              path='/report/:slug'
               exact
               element={<ProtectedRouter element={<ViewReport />} />}
             />
-            <Route path="*" element={<NotFound />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
